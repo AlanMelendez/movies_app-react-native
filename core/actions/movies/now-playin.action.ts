e07909fc6bc50562a368aebea60114b0561e3e8c
AlanMelendez <alancuevasmelendezalan123@gmail.com>
2025-03-04 22:21:18 -0600

    add: implement movie API and now playing action; include axios dependency

---
+++ core/actions/movies/now-playin.action.ts
@@ -0,0 +1,10 @@
+export const nowPlaying = async (page: number = 1) => {
+    try {
+
+        
+        
+    } catch (error) {
+        console.log(error)
+        throw "Cannot fetch now playing movies";
+    }
+}
