6ef0052e706b7c2cb641c3f2a98abb0ddf298ecd
AlanMelendez <alancuevasmelendezalan123@gmail.com>
2025-03-04 22:35:37 -0600

    add: implement now playing action and create MoviesDBMoviesResponse interface

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import '../global.css'
+import { nowPlaying } from '@/core/actions/movies/now-playin.action'
 const RootLayout = () => {
+
+  nowPlaying();
   return (
     <View className='flex flex-1 justify-center items-center'>
       <Text className='text-3xl text-cyan-600'>RootLayout</Text>
--- core/actions/movies/now-playin.action.ts
+++ core/actions/movies/now-playin.action.ts
@@ -1,7 +1,13 @@
+import { movieApi } from "@/core/api/movie-api";
+
 export const nowPlaying = async (page: number = 1) => {
     try {
 
-        
+        const {data} = await movieApi.get('/now_playing');
+
+        console.log(JSON.stringify(data.results, null, 2));
+
+        return [...data.results];
         
     } catch (error) {
         console.log(error)
