4c5da88ca96a5ff4431e277d4c4e57f91af5fa66
AlanMelendez <alancuevasmelendezalan123@gmail.com>
2025-03-09 23:46:43 -0600

    add: integrate @tanstack/react-query for data fetching in RootLayout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,26 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import '../global.css'
-import { nowPlaying } from '@/core/actions/movies/now-playin.action'
+import { View, Text } from "react-native";
+import React from "react";
+import "../global.css";
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { nowPlaying } from "@/core/actions/movies/now-playin.action";
 const RootLayout = () => {
+  // nowPlaying();
 
-  nowPlaying();
+  const queryClient = new QueryClient();
+
   return (
-    <View className='flex flex-1 justify-center items-center'>
-      <Text className='text-3xl text-cyan-600'>RootLayout</Text>
-    </View>
-  )
-}
+    <QueryClientProvider client={queryClient}>
+      <View className="flex flex-1 justify-center items-center">
+        <Text className="text-3xl text-cyan-600">RootLayout</Text>
+      </View>
+    </QueryClientProvider>
+  );
+};
 
-export default RootLayout
+export default RootLayout;
