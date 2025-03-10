78c0de0f49dadf64b6b0cc71b6111317bb43a8e1
AlanMelendez <alancuevasmelendezalan123@gmail.com>
2025-03-10 00:29:31 -0600

    add: implement loading state in HomeScreen and adjust padding for safe area

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,22 +1,29 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { useMovies } from '@/presentation/Movies/hooks/useMovies'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 const HomeScreen = () => {
  
+    const safeArea = useSafeAreaInsets();
     const {nowPlayingQuery} = useMovies();
 
-    
+    if(nowPlayingQuery.isLoading){
+      return (
+        <View className='flex-1 justify-center items-center'>
+        <View className='flex flex-row items-center space-x-4'>
+          <View className='w-6 h-6 mr-1 bg-blue-500 rounded-full animate-pulse'></View>
+          <View className='w-6 h-6 mr-1 bg-blue-500 rounded-full animate-pulse delay-150'></View>
+          <View className='w-6 h-6 mr-1 bg-blue-500 rounded-full animate-pulse delay-300'></View>
+        </View>
+      </View>
+      );
+    }
+
 
   return (
-    <View>
-      <Text>HomeScreen</Text>
-
-      <Text>
-        {
-            JSON.stringify(nowPlayingQuery.data)
-        }
-      </Text>
+    <View className='mt-4' style={{paddingTop: safeArea.top}}>
+      <Text className='text-3xl font-bold px-4 mb-2'>Home Screen</Text>
     </View>
   )
 }
