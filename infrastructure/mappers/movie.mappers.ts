929c96b57b3224985be78c0a7aeca1197fc32f91
AlanMelendez <alancuevasmelendezalan123@gmail.com>
2025-03-17 01:08:01 -0600

    add: implement movie details screen and API integration for fetching movie data

--- infrastructure/mappers/movie.mappers.ts
+++ infrastructure/mappers/movie.mappers.ts
@@ -1,5 +1,7 @@
 import { Movie } from "@/core/actions/movies/movie.interface";
 import { Result } from "../interfaces/moviedb-response";
+import { MovieDBMovieResponse } from "../interfaces/moviedb-movie.response";
+import { CompleteMovie } from "../interfaces/movie.interface";
 
 export class MovieMapper {
     static fromTheMovieDBToMovie = (movie: Result): Movie => {
@@ -12,6 +14,23 @@ export class MovieMapper {
             rating: movie.vote_average,
             poster: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
             backdrop: `https://image.tmdb.org/t/p/w500${movie.backdrop_path}`
+        }
+    }
+
+    static fromTheMovieDBToCompleteMovie = (movie: MovieDBMovieResponse) : CompleteMovie =>{
+        return {
+            id: movie.id,
+            title: movie.title,
+            description: movie.overview,
+            releaseDate: new Date(movie.release_date),
+            rating: movie.vote_average,
+            poster: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+            backdrop: `https://image.tmdb.org/t/p/w500${movie.backdrop_path}`,
+            budget : movie.budget,
+            duration: movie.runtime,
+            genres: movie.genres.map( g => g.name),
+            originalTitle: movie.original_title,
+            productionCompanies: movie.production_companies.map(c => c.name)
         }
     }
 }
